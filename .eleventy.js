e757297eb6bf33c072f8bed69337c792b83f5890
Scott McCracken <scottmccracken@gmail.com>
2022-03-08 22:52:51 -0500

    Reboot of the site with a fresh install of eleventy and very basic setup

--- .eleventy.js
+++ .eleventy.js
@@ -1,70 +1,11 @@
-require("dotenv").config();
+module.exports = config => {
+    config.addPassthroughCopy('./src/images/');
 
-const pluginRss = require("@11ty/eleventy-plugin-rss");
-const pluginNavigation = require("@11ty/eleventy-navigation");
-const markdownIt = require("markdown-it");
-const markdownItAnchor = require("markdown-it-anchor");
-const markdownItFootnote = require("markdown-it-footnote");
-
-// Variables
-const now = new Date();
-const IS_PRODUCTION = process.env.ELEVENTY_ENV === "production";
-
-// Filters
-const dateReadable = require("./src/filters/date-readable.js");
-
-module.exports = function (config) {
-    // Markdown
-    let markdownLibrary = markdownIt({
-        html: true,
-        breaks: true,
-        linkify: true,
-    })
-        .use(markdownItAnchor, {
-            permalink: true,
-            permalinkClass: "heading-permalink",
-            permalinkSymbol: "∞",
-        })
-        .use(markdownItFootnote);
-    config.setLibrary("md", markdownLibrary);
-
-    let markdownItOpts = {
-        html: true,
-        linkify: false,
-        typographer: true,
-    };
-    let markdownItAnchorOpts = {
-        permalink: true,
-        permalinkClass: "direct",
-        permalinkSymbol: "",
-    };
-
-    const md = markdownIt(markdownItOpts);
-    md.use(markdownItFootnote);
-    md.use(markdownItAnchor);
-
-    //Add Filters
-    config.addFilter("dateReadable", dateReadable);
-
-    // Plugins
-    config.addPlugin(pluginRss);
-    config.addPlugin(pluginNavigation);
-
-    // Asset Watch Targets
-    config.addWatchTarget("./src/assets");
-
-    // Base Config
     return {
-        templateFormats: ["html", "njk", "md"],
-        dataTemplateEngine: "njk",
-        htmlTemplateEngine: "njk",
-        markdownTemplateEngine: "njk",
-        passthroughFileCopy: true,
         dir: {
-            input: "src",
-            output: "dist",
-            includes: "_includes",
-            data: "_data",
+            input: 'src',
+            output: 'dist'
         },
+        passthroughFileCopy: true
     };
-};
+}
--- src/index.md
+++ src/index.md
@@ -1,5 +1,6 @@
 ---
 title: "Hi there, I'm Emily"
+lede: "An illustrator, chocolate sculptress, and aspiring art teacher in northern Vermont."
 layout: 'layouts/base.html'
 permalink: '/'
 ---
