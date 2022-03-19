85f1f50108348e426681367c458710e35ef1b1cc
scottmccracken <scott.mccracken@accessibleweb.com>
2022-03-19 11:22:46 -0400

    Add cleanCSS filter

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,15 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const CleanCSS = require("clean-css");
 
 module.exports = config => {
     config.addPlugin(eleventyNavigationPlugin);
 
     config.addPassthroughCopy('./src/fonts/');
     config.addPassthroughCopy('./src/images/');
+
+    config.addFilter("cssmin", function(code) {
+        return new CleanCSS({}).minify(code).styles;
+      });
 
     return {
         dir: {
