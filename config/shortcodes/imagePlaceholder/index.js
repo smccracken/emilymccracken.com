ed89bb8955085623b11ccb2b6ee133679ef931a9
scottmccracken <scott.mccracken@accessibleweb.com>
2023-03-21 17:11:28 -0400

    Additional widths and minified markup for generated images

--- config/shortcodes/imagePlaceholder/index.js
+++ config/shortcodes/imagePlaceholder/index.js
@@ -1,32 +1,54 @@
 const Image = require('@11ty/eleventy-img');
+const path = require('path');
+const htmlmin = require('html-minifier-terser');
 
 const imageShortcodePlaceholder = async (src, classes, alt, sizes="100vw", loading="lazy") => {
   if (alt === undefined) {
       throw new Error('Missing \'alt\` on responsiveimae from: ${src}');
   }
 
-  let metadata = await Image(src, {
-      width: [300, 600],
-      formats: ['webp', 'jpeg'],
-      outputDir: "./dist/images/",
-      urlPath: "/images/",
+    let metadata = await Image(src, {
+        widths: [400, 700, 1280],
+        formats: ['webp', 'jpeg'],
+        outputDir: "./dist/images/",
+        urlPath: "/images/",
+        filenameFormat: function (id, src, width, format, options) {
+            const extension = path.extname(src);
+            const name = path.basename(src, extension);
+      
+            return `${name}-${width}w.${format}`;
+        }
   });
 
   let lowsrc = metadata.jpeg[0];
   let highsrc = metadata.jpeg[metadata.jpeg.length - 1];
 
-  return `<picture class="${classes}">
-      ${Object.values(metadata).map(imageFormat => {
-          return `    <source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map(entry => entry.srcset).join(", ")}" sizes="${sizes}">`;
+  // getting the url  to use
+  let imgSrc = src;
+  if (!imgSrc.startsWith('.')) {
+    const inputPath = this.page.inputPath;
+    const pathParts = inputPath.split('/');
+    pathParts.pop();
+    imgSrc = pathParts.join('/') + '/' + src;
+  }
+
+  return htmlmin.minify(`<picture class="${classes}">
+      ${Object.values(metadata)
+        .map(imageFormat => {
+          return `    <source type="${imageFormat[0].sourceType}" srcset="${imageFormat
+            .map(entry => entry.srcset)
+            .join(", ")}" sizes="${sizes}">`;
       }).join("\n")}
           <img
-              src="${lowsrc.url}"
+              src="${highsrc.url}"
               width="${highsrc.width}"
               height="${highsrc.height}"
               alt="${alt}"
               loading="${loading}"
               decoding="async">
-      </picture>`;
+      </picture>`,
+        {collapseWhitespace: true}
+    );
 };
 
 module.exports = imageShortcodePlaceholder;
